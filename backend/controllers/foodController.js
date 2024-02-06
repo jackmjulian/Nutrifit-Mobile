ddc705706666ecb2019a94167c440646e9fd732c
Jack Julian <117949276+jackmjulian@users.noreply.github.com>
2024-02-06 20:07:28 +0000

    food route/controller update

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,6 +1,9 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import { Food } from '../models/foodModel.js';
 
+// All Private routes are protected by the protect middleware
+// Make sure this is added to the route in the route file
+
 // @desc Fetch all foods
 // @route GET /api/foods
 // @access Private
@@ -23,4 +26,25 @@ const getFoodById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getFoods, getFoodById };
+// @desc    Create a food
+// @route   POST /api/foods
+// @access  Private
+const createFood = asyncHandler(async (req, res) => {
+  // res.send('create food');
+
+  const { food_name, food_calories, food_protein, food_carbs, food_fat } =
+    req.body;
+  const food = new Food({
+    user: req.user._id,
+    // user,
+    food_name,
+    food_calories,
+    food_protein,
+    food_carbs,
+    food_fat,
+  });
+  const createdFood = await food.save();
+  res.status(201).json(createdFood);
+});
+
+export { getFoods, getFoodById, createFood };
--- backend/routes/foodRoutes.js
+++ backend/routes/foodRoutes.js
@@ -1,15 +1,25 @@
 import express from 'express';
 const router = express.Router();
-import { getFoods, getFoodById } from '../controllers/foodController.js';
+import {
+  getFoods,
+  getFoodById,
+  createFood,
+} from '../controllers/foodController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // TODO: Add protect middleware to protect the route from unauthorized access
 
+// All Private routes are protected by the protect middleware
+// Make sure this is added to the route in the route file
+
 // Route: /api/foods
 
-// Route for all foods
-router.route('/').get(getFoods);
+// Route for all foods & creating a new food
+router.route('/').get(getFoods).post(protect, createFood);
 
 // Route for a single food using id
-router.route('/:id').get(getFoodById);
+router.route('/:id').get(protect, getFoodById);
+
+// TODO: Add the rest of the routes
 
 export default router;
